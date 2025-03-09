4d2f7ca95a19ee783cb2c8e75c96474a0fcc6ad3
Saeed-Soukiah <saeedsoukiah@gmail.com>
2025-03-10 00:20:22 +0300

    Clock.cpp

--- Clock.cpp
+++ Clock.cpp
@@ -1,12 +1,6 @@
 /*
 Author: Saeed Soukiah
 Date: December 12, 2024
-Description: This program simulates a digital clock that displays the current time in hours, minutes, and seconds. The user can:
-- Initialize the clock with a specific time.
-- Change the time by pressing Enter.
-- Reset the clock to 00:00:00 by pressing the 'r' key.
-- Exit the program by pressing the 'e' key.
-The clock updates every second and includes input handling to allow time changes or resetting without interrupting the clock display.
 */
 
 #include <iostream>
